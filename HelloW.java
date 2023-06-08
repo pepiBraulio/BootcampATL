64d7cde417f6b49682d16bb343f61ba6b788d708
PC lichus <brauliogm16@gmail.com>
2023-06-08 13:41:15 -0600

    "Pueba con la lichus"

--- HelloW.java
+++ HelloW.java
@@ -1,5 +1,7 @@
 public class HelloW {
     public static void main(String[] args) {
         System.out.println("Hello W");
+        System.out.println("buenas");
+        System.out.println("Haciendo pruebas de git aca con la lichus");
     }
 }
