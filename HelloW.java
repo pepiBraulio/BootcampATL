44809f4577e671895693ccada6fc2d10de5e663c
PC lichus <brauliogm16@gmail.com>
2023-06-13 11:03:49 -0600

    agragando un poquito mas de salsa a los tacos

--- HelloW.java
+++ HelloW.java
@@ -2,6 +2,15 @@ public class HelloW {
     public static void main(String[] args) {
         System.out.println("Hello W");
         System.out.println("buenas");
+
+        System.out.println("Hagamos un if sencillo?");
+
+        int i = 0;
+        do {
+            i++;
+            System.out.println(i);
+        }while (i < 5);
+
         System.out.println("Haciendo pruebas de git aca con la lichus");
     }
 }
