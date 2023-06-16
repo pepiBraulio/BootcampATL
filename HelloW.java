3081a1088680e75c2abdbf0489b473c2b2bf4c36
PC lichus <brauliogm16@gmail.com>
2023-06-16 13:34:42 -0600

    "agregando el mercadito"

--- HelloW.java
+++ HelloW.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HelloW {
     public static void main(String[] args) {
         System.out.println("buenas");
@@ -12,7 +15,36 @@ public class HelloW {
 
         System.out.println("Haciendo pruebas de git aca con la lichus");
 
-        
 
+        //Mercadito
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> mercado = new ArrayList<>();
+
+        System.out.println("Buen día, usted va al mercado, cuentas frutas diferentes va a llevar?");
+        int numeroFrutas = sc.nextInt();
+
+        AgregarFutasAlArray(sc, mercado, numeroFrutas);
+
+        MostrarFrutas(mercado);
+
+
+    }
+
+    private static void AgregarFutasAlArray(Scanner sc, ArrayList<String> mercado, int numeroFrutas) {
+        System.out.println("Escriba esa frutas que busca:");
+
+        for (int j = 0; j <= numeroFrutas; j++){
+            String fruta = sc.nextLine();
+            mercado.add(fruta);
+        }
+    }
+
+    private static void MostrarFrutas(ArrayList<String> mercado) {
+        System.out.println("Las frutas que quiere son las siguientes:");
+
+        for (String frutasQueridas : mercado) {
+            System.out.println(frutasQueridas);
+        }
     }
 }
