1a628e77d83080410aa8c08165a4f80254f5cfb7
PC lichus <brauliogm16@gmail.com>
2023-06-16 12:28:16 -0600

    Pequeñoas cambios al repo, para intentar hacer el push

--- HelloW.java
+++ HelloW.java
@@ -1,6 +1,5 @@
 public class HelloW {
     public static void main(String[] args) {
-        System.out.println("Hello W");
         System.out.println("buenas");
 
         System.out.println("Hagamos un if sencillo?");
@@ -12,5 +11,8 @@ public class HelloW {
         }while (i < 5);
 
         System.out.println("Haciendo pruebas de git aca con la lichus");
+
+        
+
     }
 }
